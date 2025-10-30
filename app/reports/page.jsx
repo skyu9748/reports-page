be90dd959e487963a66bf95df305634b9715aa01
skyu9748 <skyu9748@gmail.com>
2025-10-31 02:17:42 +0900

    first commit

---
+++ app/reports/page.jsx
@@ -0,0 +1,11 @@
+export default function Reports() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>학생 주간 리포트</h1>
+      <p>테스트 점수: 95점</p>
+      <p>과제 제출: 완료</p>
+      <p>비고: 이번 주 집중도 좋음</p>
+    </div>
+  );
+}
+
